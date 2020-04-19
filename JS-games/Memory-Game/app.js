85fd1b7fd31dc0174919f35debccc38fdab7decd
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-04-19 15:03:28 -0400

    Start on app.js, initial css styling, create html boilerplate

---
+++ JS-games/Memory-Game/app.js
@@ -0,0 +1,74 @@
+//All JS goes in this EventListener
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card option
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        }
+    ]
+
+    //pick out HTML grid element with querySelector and define as grid for JS
+    const grid = document.querySelector('.grid')
+
+    //use for loop: loop over card array and for each card, create image element called 'elementCard'
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    // Resume at 6:40
+    createBoard()
+
+})
