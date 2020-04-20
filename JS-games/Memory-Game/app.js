379ef7a44d8649cbe6ffb1800c8c527a208a4556
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-04-20 11:26:03 -0400

    Finish javascript to implement game functionality. Need additional styling.

--- JS-games/Memory-Game/app.js
+++ JS-games/Memory-Game/app.js
@@ -52,23 +52,67 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'pizza',
             img: 'images/pizza.png'
         }
-    ]
+    ];
+
+    // Randomize card location by randomizing the array
+    cardArray.sort(() => 0.5 - Math.random());
 
     //pick out HTML grid element with querySelector and define as grid for JS
-    const grid = document.querySelector('.grid')
+
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result');
+    //create empty array for selected cards
+    var cardsChosen = [];
+    var cardsChosenId = [];
+    var cardsWon = [];
 
     //use for loop: loop over card array and for each card, create image element called 'elementCard'
     function createBoard() {
         for (let i = 0; i < cardArray.length; i++) {
-            var card = document.createElement('img')
-            card.setAttribute('src', 'images/blank.png')
-            card.setAttribute('data-id', i)
-            //card.addEventListener('click', flipcard)
-            grid.appendChild(card)
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        };
+    };
+
+    // check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img');
+        const optionOneId = cardsChosenId[0];
+        const optionTwoId = cardsChosenId[1];
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match!');
+            cards[optionOneId].setAttribute('src', 'images/white.png');
+            cards[optionTwoId].setAttribute('src', 'images/white.png');
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png');
+            cards[optionTwoId].setAttribute('src', 'images/blank.png');
+            alert('Sorry! No match. Try again!');
+        };
+        cardsChosen = [];
+        cardsChosenId = [];
+        resultDisplay.textContent = cardsWon.length;
+        if (cardsWon.length === cardArray.length/2) {
+            resultDisplay.textContent = 'Congratulations. You found all the matches!';
         }
     }
 
-    // Resume at 6:40
-    createBoard()
 
-})
+    // flip cards
+    function flipCard() {
+        var cardId = this.getAttribute('data-id');
+        cardsChosen.push(cardArray[cardId].name);
+        cardsChosenId.push(cardId);
+        this.setAttribute('src', cardArray[cardId].img);
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500);
+        }
+    };
+
+
+    createBoard();
+
+});
--- JS-games/README.md
+++ JS-games/README.md
@@ -1,3 +1,5 @@
+# JS-Games
+
 7 Simple grid-based JavaScript games.
 
 These are created based on a FreeCodeCamp tutorial available on YouTube: https://youtu.be/lhNdUVh3qCc
