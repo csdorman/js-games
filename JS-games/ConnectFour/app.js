1b3c5e3239af5829cf63f499cf3a091502e83df1
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-05-08 11:12:15 -0400

    Found bug in app.js that was preventing the click detection: misplaced ;

--- JS-games/ConnectFour/app.js
+++ JS-games/ConnectFour/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const square = document.querySelectorAll('.grid div');
+    console.log('DOM loaded');
+    const squares = document.querySelectorAll('.grid div');
     const result = document.querySelector('#result');
     const displayCurrentPlayer = document.querySelector('#current-player');
     let currentPlayer = 1;
@@ -7,41 +8,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //display player 1 on load
     displayCurrentPlayer.innerHTML = currentPlayer;
-    // loop through each square
-    for (var i = 0, len = square.length; i < len; i++); {
-        // add onclick to each square
-        //Attempting to try and get clicks to work ANYWHERE!
-        document.getElementByClass('square').onclick = function notify(){
-            alert('Page click detected');   
-        }
-        // End click testing attempt
-        (function(index){
-            square[i].onclick = function() {
-                //if square below selected square(+ 7 in div row) is taken, space is valid
-                if (square[i+7].classList.contains('taken')){
-                    //if current player is player-one
-                    if (currentPlayer === 1) {
-                        // add class 'taken' and 'player-one' to selected square
-                        square[index].classList.add('taken');
-                        square[index].classList.add('player-one');
-                        // change the player
-                        currentPlayer = 2;
-                        displayCurrentPlayer.innerHTML = currentPlayer;
-                    // if current player is player-two
-                    } else if (currentPlayer === 2) {
-                        // add class 'taken' and 'player-two' to selected square
-                        square[index].classList.add('taken');
-                        square[index].classList.add('player-two');
-                        // change the player
-                        currentPlayer = 1;
-                        displayCurrentPlayer.innerHTML = currentPlayer;
-                    };
-                } else {
-                    alert('This is not a valid space!');
+    // loop through each squares
+    for (var i = 0, len = squares.length; i < len; i++) {
+        // add onclick to each squares
+       
+    (function(index){
+        squares[i].onclick = function(){
+            //if squares below selected squares(+ 7 in div row) is taken, space is valid
+            if(squares[index + 7].classList.contains('taken')){
+                //if current player is player-one
+                if (currentPlayer === 1) {
+                    // add class 'taken' and 'player-one' to selected squares
+                    squares[index].classList.add('taken');
+                    squares[index].classList.add('player-one');
+                    // change the player
+                    currentPlayer = 2;
+                    displayCurrentPlayer.innerHTML = currentPlayer;
+                // if current player is player-two
+                } else if (currentPlayer === 2) {
+                    // add class 'taken' and 'player-two' to selected squares
+                    squares[index].classList.add('taken');
+                    squares[index].classList.add('player-two');
+                    // change the player
+                    currentPlayer = 1;
+                    displayCurrentPlayer.innerHTML = currentPlayer;
                 };
-            };
-        });
-    };
+            } else  alert('This is not a valid space!');
+        };
+    })(i);
+    }
 
 
     
