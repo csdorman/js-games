51e1b7fb54c5f198d20bbb42260d0355cbdf81e2
Colin Dorman <csdorman@Colins-MacBook-Pro.local>
2020-05-19 10:45:59 -0400

    More game code: snake movement, startGame, work on outcomes

--- JS-games/Snake/app.js
+++ JS-games/Snake/app.js
@@ -19,6 +19,64 @@ document.addEventListener('DOMContentLoaded', () => {
     let intervalTime = 0;
     let interval = 0;
 
-    //set keycodes for snake movement
+    // start (and restart) the game
+    function startGame() {
+        currentSnake.forEach(index => squares[index].classList.remove('snake'));
+        squares[appleIndex].classList.remove('apple');
+        clearInterval(interval);
+        score = 0;
+        //function randomApple()
+        direction = 1;
+        scoreDisplay.innerText = score;
+        intervalTime = 1000;
+        currentSnake = [2,1,0];
+        currentIndex = 0;
+        currentSnake.forEach(index => square[index].classList.add('snake'));
+        interval = setInterval(moveOutcomes, intervalTime);
+    }
 
+    // function for dealing with all outcomes of Snake
+    function moveOutcomes() {
+        // snake hitting border and self
+        if (
+            (currentSnake[0] + width >= (width * width) && direction === width) || //snake hitting bottom
+                (currentSnake[0] % width === width -1 && direction === 1) || //snake hit right wall
+                (currentSnake[0] % width === 0 && direction === -1) || //snake hit left wall
+                (currentSnake[0] - width < 0 && direction === -width) || //snake hits top
+            squares[currentSnake[0] + direction].classList.contains('snake'); //snake runs into itself
+        ) {
+            return clearInterval(interval); //if any of the above happen, clear interval
+        }
+        const tail = currentSnake.pop(); //removes the last item of the array and shows it
+        squares[tail].classList.remove('snake'); //removes 'snake' class from TAIL
+        currentSnake.unshift(currentSnake[0] + direction); //gives direction to head
+        // deal with snake getting the apple
+        if(squares[currentSnake[0]].classList.contains('apple')) {
+            squares[currentSnake[0]].classList.remove('apple');
+            squares[tail].classList.add('snake');
+            currentSnake.push(tail);
+            //randomApple()
+            score++
+            
+        }
+    }
+
+
+
+    //set keycodes for snake movement
+    function control(e) {
+        squares[currentIndex].classList.remove('snake');
+
+        if(e.keyCode === 39) {
+            direction = 1; //right arrow key moves snake 1 div right
+        } else if (e.keyCode === 38) {
+            direction = -width; //up arrow snake moves up 1 row width
+        } else if (e.keyCode === 37) {
+            direction = -1; //left arrow moves snake 1 div left
+        } else if (e.keyCode === 40) {
+            direction = +width; // down arrow moves down 1 row width
+        }
+    };
+
+    document.addEventListener('keyup', control)
 });
