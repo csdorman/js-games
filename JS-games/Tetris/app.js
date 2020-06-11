3112fd52eded0b3446d6c1ba804020db6abe1df6
Colin Dorman <csdorman@gmail.com>
2020-06-11 11:20:07 -0400

    Create initial files for Tetris. Create HTML structure. Start on JS.

---
+++ JS-games/Tetris/app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // define the grid as the html div
+    const grid = document.querySelector('.grid');
+    // create an array from all the html divs in .grid
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const scoreDisplay = document.querySelector('#score');
+    const startBtn = document.querySelector('#start-button');
+    // define the width of the board
+    const width = 10;
+
+});
+
