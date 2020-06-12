1af9524765feed09947c1cd102ce432fb5b7bf3b
Colin Dorman <csdorman@gmail.com>
2020-06-12 13:46:18 -0400

    Starting on creating the tetrominos.

--- JS-games/Tetris/app.js
+++ JS-games/Tetris/app.js
@@ -8,5 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
     // define the width of the board
     const width = 10;
 
+    // The Tetromino shapes
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 +2],
+        [1, width +1, width * 2, width * 2 +1],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ];
+    // Use guide at 35:10 to create these arrays
+    const zTetromino = [];
+    const tTetromino = [];
+    const oTetromino = [];
+    const iTetromino = [];
+    // Resume video at 37:45 after arrays are done
 });
 
